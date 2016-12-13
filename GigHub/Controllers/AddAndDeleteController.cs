b0d40ce569e3d452f376215523de660883c8fa96
E5440-BQRKL32\Administrator <simbahmso@yahoo.com>
2016-12-12 23:45:31 -0700

    Testing Git Repo by creating useless controller
    Testing Git Repo by creating useless controller

---
+++ GigHub/Controllers/AddAndDeleteController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GigHub.Controllers
+{
+    public class AddAndDeleteController : Controller
+    {
+        // GET: AddAndDelete
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
