272619a794fe96efedc044fc2be16a61d651976d
E5440-BQRKL32\Administrator <simbahmso@yahoo.com>
2016-12-13 11:55:52 -0700

    Added Genre dropdown list to Form
    Created Genres property and set it as integer because dropdowns accept
    integers and it will be stored as GenreId in the database!
    
    Not worried about Dependancy Injection or Repositories at this point as
    you can see because controller is dealing with DAL - Not good SOLID
    practice! - Will change it later

--- GigHub/Controllers/GigsController.cs
+++ GigHub/Controllers/GigsController.cs
@@ -1,13 +1,26 @@
 ﻿using System.Web.Mvc;
+using GigHub.Models;
+using GigHub.ViewModels;
 
 namespace GigHub.Controllers
 {
     public class GigsController : Controller
     {
+        private ApplicationDbContext _context;
+
+        public GigsController()
+        {
+            _context = new ApplicationDbContext(); //Don't worry about Dependancy Injection
+                                        // or Repositories at this point !
+        }
         // GET: Gigs
         public ActionResult Create()
         {
-            return View();
+            var viewModel = new GigFormViewModel
+            {
+                Genres = _context.Genres.ToList()
+            };
+            return View(viewModel);
         }
     }
 }
--- GigHub/ViewModels/GigFormViewModel.cs
+++ GigHub/ViewModels/GigFormViewModel.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using GigHub.Models;
 
 namespace GigHub.ViewModels
 {
@@ -9,6 +10,8 @@ namespace GigHub.ViewModels
     {
         public string Venue { get; set; }
         public string Date { get; set; }
-        public string Time { get; set; }    
+        public string Time { get; set; }
+        public int Genre { get; set; }
+        public IEnumerable<Genre> Genres { get; set; }
     }
 }
