93b93f1eb36118fd17414746517412724ff595f5
E5440-BQRKL32\Administrator <simbahmso@yahoo.com>
2016-12-13 10:26:48 -0700

    Fixed duplicate ID in database
    PROBLEM:
    Violation of PRIMARY KEY constraint 'PK_dbo.Genres'. Cannot insert
    duplicate key in object 'dbo.Genres'. The duplicate key value is (1).
    
    SOLUTION:
    gave unique Id to a record in Genres Table

--- GigHub/Migrations/201612131646403_PopulateGenresTable.cs
+++ GigHub/Migrations/201612131646403_PopulateGenresTable.cs
@@ -2,7 +2,7 @@ namespace GigHub.Migrations
 {
     using System;
     using System.Data.Entity.Migrations;
-
+    
     public partial class PopulateGenresTable : DbMigration
     {
         public override void Up()
@@ -10,7 +10,7 @@ namespace GigHub.Migrations
             Sql("INSERT INTO Genres(Id, Name) VALUES (1, 'Jazz')");
             Sql("INSERT INTO Genres(Id, Name) VALUES (2, 'Blues')");
             Sql("INSERT INTO Genres(Id, Name) VALUES (3, 'Rock')");
-            Sql("INSERT INTO Genres(Id, Name) VALUES (1, 'Country')");
+            Sql("INSERT INTO Genres(Id, Name) VALUES (4, 'Country')");
         }
 
         public override void Down()
