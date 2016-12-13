31afd6ef2ff3c9ab5b169708fb34c4e63c41a0bb
E5440-BQRKL32\Administrator <simbahmso@yahoo.com>
2016-12-13 13:32:22 -0700

    Saving Data From Form
    Saving Data From Form - No validation at this point, assuming everything
    is validated from user!
    
    So we have a Gig object and we need to add it to our _context so it can
    be tracked by EF and we call save changes() method and EF will generate
    a SQL statement and execute it against the database.
    
    Temporarily, we are going to redirect user to the Home Page and later in
    the next Use Cases i will replace the Home page with a list of all the
    upcoming gigs!

--- GigHub/Controllers/GigsController.cs
+++ GigHub/Controllers/GigsController.cs
@@ -1,7 +1,9 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using System.Web.Mvc;
 using GigHub.Models;
 using GigHub.ViewModels;
+using Microsoft.AspNet.Identity;
 
 namespace GigHub.Controllers
 {
@@ -23,6 +25,28 @@ namespace GigHub.Controllers
             };
 
             return View(viewModel);
+        }
+
+        [Authorize, HttpPost]
+        public ActionResult Create (GigFormViewModel viewModel )
+        {
+            var artist = _context.Users.Single(u => u.Id == User.Identity.GetUserId());
+            var genre = _context.Genres.Single(g => g.Id == viewModel.Genre);
+
+            var gig = new Gig
+
+            {
+                Artist = artist,
+                DateTime = DateTime.Parse(string.Format("{0}{1}", viewModel.Date, viewModel.Time)),
+                Genre = genre,
+                Venue = viewModel.Venue
+
+            };
+
+            _context.Gigs.Add(gig);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index", "Home");
         }
     }
 }
